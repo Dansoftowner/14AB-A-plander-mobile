c94f65a160b10727f911d28aa9a5cd3797f7e77c
KubikArmin <kubikarminmartin@gmail.com>
2024-02-01 08:42:54 +0100

    :lipstick: Improve styles, colors

---
+++ config/colors.js
@@ -0,0 +1,14 @@
+export default {
+    soft_blue: "#89d0df",
+    light_blue: "#3393df",
+    medium_blue: "#0078d7",
+    dark_blue: "#004881",
+    black: "#000",
+    white: "#fff",
+    medium: "#6e6969",
+    light: "#f8f4f4",
+    dark: "#0c0c0c",
+    danger: "#ff5252",
+    yellow: "#004881",
+  };
+  
