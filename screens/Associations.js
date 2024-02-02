e76953a3b5bc288ca0d248700ec4ff7561f4388b
KubikArmin <kubikarminmartin@gmail.com>
2024-02-02 11:51:31 +0100

    :construction: Implement associations

---
+++ screens/Associations.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import { View, StyleSheet } from 'react-native'
+// import MyButton from '../components/MyButton'
+// import getAssociations from '../api/associations'
+
+// function Associations(props) {
+//   const handleOnPress = () => {
+//     // const associations = getAssociations()
+//     console.log(associations)
+//   }
+//   return (
+//     <Screen>
+//       <View style={styles.container}>
+//         <MyButton title="Get Associations" onPress={handleOnPress} />
+//       </View>
+//     </Screen>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   container: {},
+// })
+
+// export default Associations
