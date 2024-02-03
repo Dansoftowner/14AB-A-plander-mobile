9248240c17b147248052aebb911e266c1b9c2077
KubikArmin <kubikarminmartin@gmail.com>
2024-02-03 19:08:20 +0100

    :recycle: Import renamed components

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -2,28 +2,27 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import Screen from './Screen'
 import MyText from '../components/MyText'
-import { useTheme } from '@react-navigation/native';
-import colors from '../config/colors';
-import useAuth from '../auth/useAuth';
-import LogOut from '../components/LogOut';
-import DarkMode from '../components/DarkModeSwitch';
+import { useTheme } from '@react-navigation/native'
+import colors from '../config/colors'
+import useAuth from '../auth/useAuth'
+import ThemeSwitch from '../components/ThemeSwitch'
+import LanguageSelector from '../components/LanguageSelector'
+import Logout from '../components/LogOut'
 
 function MainScreen(props) {
-    const {user} = useAuth();
-
   return (
     <Screen>
       <View style={[styles.container]}>
-      <LogOut />
-      <DarkMode/>
+        <Logout />
+        <ThemeSwitch />
+        <LanguageSelector />
       </View>
     </Screen>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-  }
+  container: {},
 })
 
 export default MainScreen
