a1a14462aa062ef6b20380c638ea85c745830a5a
KubikArmin <kubikarminmartin@gmail.com>
2024-02-04 21:39:58 +0100

    :bulb: Comment useless code

--- api/client.js
+++ api/client.js
@@ -12,14 +12,14 @@ apiClient.addAsyncRequestTransform(async (request) => {
     request.headers["x-auth-token"] = token;
 })
 
-apiClient.get = async (url, params, axiosConfig) => {
-    const response = await get(url, params, axiosConfig)
-    if (response.ok) {
-        // cache.store(url, response.data)
-        return response;
-    }
-    // const data = await cache.get(url);
-    // return data ? {ok: true, data} : response;
-}
+// apiClient.get = async (url, params, axiosConfig) => {
+//     const response = await get(url, params, axiosConfig)
+//     if (response.ok) {
+//         // cache.store(url, response.data)
+//         return response;
+//     }
+//     // const data = await cache.get(url);
+//     // return data ? {ok: true, data} : response;
+// }
 
 export default apiClient;
