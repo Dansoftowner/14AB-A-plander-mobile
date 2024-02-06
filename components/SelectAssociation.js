2d4feeaf35e7e856da37093a13779ff1437b6cdd
KubikArmin <kubikarminmartin@gmail.com>
2024-02-06 22:42:26 +0100

    :recycle: Refactor, delete unused imports,

--- components/SelectAssociation.js
+++ components/SelectAssociation.js
@@ -1,17 +1,13 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
-import MyFormField from './MyFormField';
-import i18n from '../locales/i18n';
-import MyText from './MyText';
 import MyTextInput from './MyTextInput';
-import AssContext from '../AssContext';
 
-function SelectAssociation({onPress}) {
-  const {association, setAssociation} = useContext(AssContext);
+function SelectAssociation({onPress, title}) {
+
   return (
     <View style={styles.container}>
         <TouchableWithoutFeedback onPress={onPress}>
-            <MyTextInput value={association.name ?? ''} title={association.name ?? 'Association'} icon='police-badge-outline' isButton={true}/>
+            <MyTextInput title={title} icon='police-badge-outline' isButton={true}/>
         </TouchableWithoutFeedback>
     </View>
   );
