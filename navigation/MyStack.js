416cea9584129bd634f7b7947e1bddd5eeda801e
KubikArmin <kubikarminmartin@gmail.com>
2024-02-06 22:47:11 +0100

    :sparkles: Enable header and back button for AssociationsScreen

--- navigation/MyStack.js
+++ navigation/MyStack.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import {createStackNavigator} from "@react-navigation/stack";
-import LoginScreen from '../screens/LoginScreen';
-import AssociationsScreen from '../screens/AssociationsScreen';
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
+import LoginScreen from '../screens/LoginScreen'
+import AssociationsScreen from '../screens/AssociationsScreen'
+import { useTheme } from '@react-navigation/native'
+import { FormProvider } from '../components/FormContext'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 function MyStack(props) {
+  const { colors: colorsByTheme } = useTheme()
   return (
-  <Stack.Navigator screenOptions={{headerShown: false}}>
-    <Stack.Screen
-      name="Login"
-      component={LoginScreen}
-    />
-    <Stack.Screen
-      name="Associations"
-      component={AssociationsScreen}
-    />
-  </Stack.Navigator>
-  );
+    <FormProvider>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Associations"
+          component={AssociationsScreen}
+          options={{
+            headerShown: true,
+            headerTitle: '',
+            headerTintColor: colorsByTheme.white_dark_blue,
+            headerStyle: { backgroundColor: colorsByTheme.Login_background },
+          }}
+          // screenOptions={{headerShown: true}}
+        />
+      </Stack.Navigator>
+    </FormProvider>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {}
-});
+  container: {},
+})
 
-export default MyStack;
+export default MyStack
