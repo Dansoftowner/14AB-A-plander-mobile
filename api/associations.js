46603ea5fc5ffcccd48520ddd3bd0213664bd394
KubikArmin <kubikarminmartin@gmail.com>
2024-02-08 22:26:27 +0100

    :globe_with_meridians:  Added language-header to get internatinalized messages from the backend

--- api/associations.js
+++ api/associations.js
@@ -1,9 +1,10 @@
+import i18n from "../locales/i18n";
 import apiClient from "./client";
 
 // const token = getToken();
 
 //const associations = await apiClient.get("/associations", { headers: { "x-auth-token": token } });
-const getAssociations = (q) => apiClient.get("/associations", {q:  q});
+const getAssociations = (q) => apiClient.get("/associations", {q:  q}, {headers: {"Accept-Language": i18n.locale}});
 
 export default {
     getAssociations
--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,7 @@
+import i18n from "../locales/i18n";
 import apiClient from "./client";
 
-const login = (associationId, user, password) => apiClient.post("/auth", { associationId, user, password });
+const login = (associationId, user, password) => apiClient.post("/auth", { associationId, user, password }, {headers: {"Accept-Language": i18n.locale}});
 
 export default {
     login
