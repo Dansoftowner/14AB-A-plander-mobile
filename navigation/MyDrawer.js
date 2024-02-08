96fafdae5fa3cdb115adae3d095441b5f5fbf6c6
KubikArmin <kubikarminmartin@gmail.com>
2024-02-08 22:44:13 +0100

    :art: Add FormProvider, format code

--- navigation/MyDrawer.js
+++ navigation/MyDrawer.js
@@ -6,37 +6,42 @@ import SettingsScreen from '../screens/SettingsScreen'
 import useAuth from '../auth/useAuth'
 import { useTheme } from '@react-navigation/native'
 import ProfileScreen from '../screens/ProfileScreen'
-
+import { FormProvider } from '../components/FormContext'
 
 const Drawer = createDrawerNavigator()
 
 function MyDrawer(props) {
-    const { colors: colorsByTheme } = useTheme()
-    const {user} = useAuth();
-    return (
-    <Drawer.Navigator
-      initialRouteName="Main"
-      screenOptions={{
-        headerShown: true,
-        headerTitle: user ? user.name : 'Plander',
-        headerTintColor: colorsByTheme.white_white,
-        headerStyle: { backgroundColor: colorsByTheme.medium_blue_dark_blue },
-        headerTitleStyle: {fontWeight: 'bold'},
-        drawerActiveTintColor: colorsByTheme.white_white,
-        drawerActiveBackgroundColor: colorsByTheme.medium_blue_dark_blue,
-      }}
-    >
-      <Drawer.Screen name="Main" component={MainScreen} />
-      <Drawer.Screen name="Settings" component={SettingsScreen} />
-      <Drawer.Screen name="Profile" component={ProfileScreen} options={{headerTitle: 'Edit Profile'}} />
-    </Drawer.Navigator>
+  const { colors: colorsByTheme } = useTheme()
+  const { user } = useAuth()
+  
+  return (
+    <FormProvider>
+      <Drawer.Navigator
+        initialRouteName="Main"
+        screenOptions={{
+          headerShown: true,
+          headerTitle: user ? user.name : 'Plander',
+          headerTintColor: colorsByTheme.white_white,
+          headerStyle: { backgroundColor: colorsByTheme.medium_blue_dark_blue },
+          headerTitleStyle: { fontWeight: 'bold' },
+          drawerActiveTintColor: colorsByTheme.white_white,
+          drawerActiveBackgroundColor: colorsByTheme.medium_blue_dark_blue,
+        }}
+      >
+        <Drawer.Screen name="Main" component={MainScreen} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+        <Drawer.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ headerTitle: 'Edit Profile' }}
+        />
+      </Drawer.Navigator>
+    </FormProvider>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
+  container: {},
 })
 
 export default MyDrawer
