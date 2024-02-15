e6560af6a6d5a66987dd4d6caf071eabb75ca6d0
KubikArmin <kubikarminmartin@gmail.com>
2024-02-15 09:58:02 +0100

    :sparkles: Added withCredentials and interceptor

--- api/client.js
+++ api/client.js
@@ -2,9 +2,9 @@ import { create } from "apisauce";
 import storage from "../auth/storage";
 import i18n from "../locales/i18n";
 
-
 const apiClient = create({
-    baseURL: "https://dev-plander-org.koyeb.app/api"
+    baseURL: "https://dev-plander-org.koyeb.app/api",
+    withCredentials: true,
 })
 
 apiClient.addAsyncRequestTransform(async (request) => {
@@ -13,9 +13,12 @@ apiClient.addAsyncRequestTransform(async (request) => {
     // if (!token) {
     //     return
     // }
-    //request.headers["x-plander-auth"] = storage.getToken()
+    request.headers["x-plander-auth"] = storage.getToken()
     request.headers["Accept-Language"] = i18n.locale
 })
+
+// const get = apiClient.get;
+
 
 // apiClient.get = async (url, params, axiosConfig) => {
 //     const response = await get(url, params, axiosConfig)
