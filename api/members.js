c672fb825da0f4e7fbeeeed3140d8d82a46b4cf9
KubikArmin <kubikarminmartin@gmail.com>
2024-02-15 09:59:14 +0100

    :recycle: Complete headers

--- api/members.js
+++ api/members.js
@@ -5,13 +5,13 @@ import apiClient from './client'
 //const associations = await apiClient.get("/associations", { headers: { "x-auth-token": token } });
 const token = storage.getToken()
 const getMe = (memberId) =>
-  apiClient.get(`/members/${memberId}`, {}, {headers: {"x-plander-auth": token}}).then(res => res)
+  apiClient.get(`/members/${memberId}`).then(res => res)
 
-  const patchMe = (name, address, idNumber, phoneNumber, guardNumber) => apiClient.patch(`/members/me`, {name, address, idNumber, phoneNumber, guardNumber}, {headers: {"x-plander-auth": token}})
+  const patchMe = (name, address, idNumber, phoneNumber, guardNumber) => apiClient.patch(`/members/me`, {name, address, idNumber, phoneNumber, guardNumber}).then(res => res)
 
-  const patchMeCredentials = (email, username, newPwd, user, currentPassword) => {
-    apiClient.patch(`/members/me/credentials`, {email, username, newPwd}, {headers: {"x-plander-auth": token}, "x-current-pass": currentPassword}).then(res => res)
-}
+  const patchMeCredentials = (email, username, newPwd, currentPassword) =>
+    apiClient.patch(`/members/me/credentials`, {email, username, newPwd}, {headers: {"x-current-pass": currentPassword}}).catch(err => err)
+    //console.log(currentPassword)
 
 export default {
   getMe,
