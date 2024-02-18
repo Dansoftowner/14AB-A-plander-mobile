f1dc41d0b77696d3895ec981a7c4a99ba39b2380
KubikArmin <kubikarminmartin@gmail.com>
2024-02-18 19:41:12 +0100

    :boom: Customized AgendaItem component

--- components/calendar/AgendaItem.js
+++ components/calendar/AgendaItem.js
@@ -1,79 +1,93 @@
-import isEmpty from 'lodash/isEmpty';
-import React, {useCallback} from 'react';
-import {StyleSheet, Alert, View, Text, TouchableOpacity, Button} from 'react-native';
+import isEmpty from 'lodash/isEmpty'
+import React, { useCallback } from 'react'
+import {
+  StyleSheet,
+  Alert,
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+  TouchableWithoutFeedback,
+} from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useTheme } from '@react-navigation/native'
 
-const AgendaItem = (props) => {
-  const {item} = props;
 
-  const buttonPressed = useCallback(() => {
-    Alert.alert('Show me more');
-  }, []);
 
-  const itemPressed = useCallback(() => {
-    Alert.alert(item.title);
-  }, []);
+export default function AgendaItem({ item, onPress, onItemPress, dotColor = "blue" }) {
+  // const buttonPressed = useCallback(() => {
+  //   Alert.alert('Show me more');
+  // }, []);
+
+  // const itemPressed = useCallback(() => {
+  //   Alert.alert(item.title);
+  // }, []);
+
+  const { colors: colorsByTheme } = useTheme()
 
   if (isEmpty(item)) {
     return (
       <View style={styles.emptyItem}>
         <Text style={styles.emptyItemText}>No Events Planned Today</Text>
       </View>
-    );
+    )
   }
 
   return (
-    <TouchableOpacity onPress={itemPressed} style={styles.item}>
-      <View>
-        <Text style={styles.itemHourText}>{item.hour}</Text>
-        <Text style={styles.itemDurationText}>{item.duration}</Text>
+    <TouchableOpacity onPress={onItemPress} style={styles.item}>
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <View
+          style={{
+            backgroundColor: dotColor,
+            width: 15,
+            height: 15,
+            borderRadius: 8,
+          }}
+        ></View>
       </View>
-      <Text style={styles.itemTitleText}>{item.title}</Text>
-      <View style={styles.itemButtonContainer}>
-        <Button color={'grey'} title={'Info'} onPress={buttonPressed}/>
+      <View
+        style={{
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={styles.itemTitleText}>{item.title}</Text>
+      </View>
+      <View
+        style={{ justifyContent: 'center', alignItems: 'flex-end', flex: 1 }}
+      >
+      <TouchableWithoutFeedback onPress={onPress} >
+        <MaterialCommunityIcons name="pencil" size={15} color={colorsByTheme.black_white} />
+      </TouchableWithoutFeedback>
       </View>
     </TouchableOpacity>
-  );
-};
-
-export default React.memo(AgendaItem);
-
+  )
+}
 
 const styles = StyleSheet.create({
   item: {
-    padding: 20,
+    padding: 10,
     backgroundColor: 'white',
     borderBottomWidth: 1,
     borderBottomColor: 'lightgrey',
-    flexDirection: 'row'
-  },
-  itemHourText: {
-    color: 'black'
-  },
-  itemDurationText: {
-    color: 'grey',
-    fontSize: 12,
-    marginTop: 4,
-    marginLeft: 4
+    flexDirection: 'row',
   },
   itemTitleText: {
     color: 'black',
-    marginLeft: 16,
+    paddingHorizontal: 10,
     fontWeight: 'bold',
-    fontSize: 16
-  },
-  itemButtonContainer: {
-    flex: 1,
-    alignItems: 'flex-end'
+    fontSize: 16,
   },
   emptyItem: {
     paddingLeft: 20,
     height: 52,
     justifyContent: 'center',
     borderBottomWidth: 1,
-    borderBottomColor: 'lightgrey'
+    borderBottomColor: 'lightgrey',
   },
   emptyItemText: {
     color: 'lightgrey',
-    fontSize: 14
-  }
-});
+    fontSize: 14,
+  },
+})
