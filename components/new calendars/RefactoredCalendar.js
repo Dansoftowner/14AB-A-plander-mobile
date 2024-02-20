20be293db90c9b3ea05d6dad2eb6a623e4c62413
KubikArmin <kubikarminmartin@gmail.com>
2024-02-20 19:51:44 +0100

    :construction: Periods and dots problem will be fixed in the future

--- components/new calendars/RefactoredCalendar.js
+++ components/new calendars/RefactoredCalendar.js
@@ -1,4 +1,10 @@
-import React, { useRef, useCallback, useState, useEffect } from 'react'
+import React, {
+  useRef,
+  useCallback,
+  useState,
+  useEffect,
+  useContext,
+} from 'react'
 import { StyleSheet } from 'react-native'
 import {
   AgendaList,
@@ -13,6 +19,7 @@ import colors from '../../config/colors'
 import assignments from '../../api/assignments'
 import MyButton from '../MyButton'
 import MyText from '../MyText'
+import AuthContext from '../../auth/authContext'
 
 LocaleConfig.locales['hu'] = {
   monthNames: [
@@ -56,22 +63,25 @@ LocaleConfig.locales['hu'] = {
   today: 'Ma',
 }
 
-export default function RefactoredCalendar(props) {
+export default function RefactoredCalendar({navigation}) {
   const leftArrowIcon = require('../../assets/arrows/previous.png')
   const rightArrowIcon = require('../../assets/arrows/next.png')
   const [markedDays, setMarkedDays] = useState(null)
   const [agendaItems, setAgendaItems] = useState(null)
   const { colors: colorsByTheme } = useTheme()
   const periodColor = colors.light_green
-  const dotColor = colors.light_yellow
+  const dotColor = colors.black
+
+  const { user, setUser } = useContext(AuthContext)
+
   const calendarTheme = {
     backgroundColor: colorsByTheme.white_black,
     calendarBackground: colorsByTheme.white_black,
     textSectionTitleColor: '#b6c1cd',
     selectedDayBackgroundColor: colors.soft_blue,
     selectedDayTextColor: '#ffffff',
-    todayBackgroundColor: colorsByTheme.light_blue_dark_blue,
-    todayTextColor: colorsByTheme.white_black,
+    //todayBackgroundColor: colorsByTheme.light_blue_dark_blue,
+    //todayTextColor: colorsByTheme.white_black,
     dayTextColor: 'black',
     textDisabledColor: colors.light,
     arrowColor: colorsByTheme.medium_blue_yellow,
@@ -117,12 +127,19 @@ export default function RefactoredCalendar(props) {
         if (i.type == 'dot') {
           markedDays[i.date] = i.marking
         } else {
-          markedDays[i.dateStart] = i.markingStart
-          markedDays[i.dateEnd] = i.markingEnd
-          console.log(i.betweenDays.length)
-          i.betweenDays.forEach((markedDay) => {
-            markedDays[markedDay.date] = markedDay.marking
-          })
+          if (i.isOneDay) {
+            markedDays[i.start] = i.marking
+          }
+          if (i.betweenDays) {
+            markedDays[i.dateStart] = i.markingStart
+            markedDays[i.dateEnd] = i.markingEnd
+            console.log(i.betweenDays.length)
+            i.betweenDays.forEach((markedDay) => {
+              markedDays[markedDay.date] = markedDay.marking
+            })
+          } else {
+            markedDays[i.start] = i.marking
+          }
         }
       })
       setMarkedDays(markedDays)
@@ -134,7 +151,55 @@ export default function RefactoredCalendar(props) {
     const endFormattedString = formatDate(new Date(assignment.end))
     const startDate = new Date(assignment.start)
     const endDate = new Date(assignment.end)
-    if (endFormattedString === startFormattedString) {
+    const assignees = assignment.assignees
+    console.log(assignment.assignees, assignment.start)
+    const isAssigned = assignees
+      .map((ass) => ass._id == user._id)
+      .includes(true)
+    console.log(isAssigned)
+    if (isAssigned) {
+      if (endFormattedString === startFormattedString) {
+        const eventObject = {
+          type: 'period',
+          isOneDay: true,
+          start: startFormattedString,
+          marking: {
+            // marked: true,
+            startingDay: startFormattedString,
+            endingDay: startFormattedString,
+            color: periodColor,
+          },
+        }
+        return eventObject
+      } else {
+        const eventObject = {
+          type: 'period',
+          isOneDay: false,
+          dateStart: startFormattedString,
+          dateEnd: endFormattedString,
+          markingStart: {
+            startingDay: true,
+            color: periodColor,
+          },
+          markingEnd: {
+            endingDay: true,
+            color: periodColor,
+          },
+          betweenDays: [],
+        }
+        const daysBetweenStartEnd = getDaysDifference(startDate, endDate)
+        for (let i = 1; i < daysBetweenStartEnd; i++) {
+          const betweenDay = {
+            date: formatDate(addDays(startDate, i)),
+            marking: {
+              color: periodColor,
+            },
+          }
+          eventObject.betweenDays.push(betweenDay)
+        }
+        return eventObject
+      }
+    } else {
       const eventObject = {
         type: 'dot',
         date: startFormattedString,
@@ -144,32 +209,32 @@ export default function RefactoredCalendar(props) {
         },
       }
       return eventObject
-    } else {
-      const eventObject = {
-        type: 'period',
-        dateStart: startFormattedString,
-        dateEnd: endFormattedString,
-        markingStart: {
-          startingDay: true,
-          color: periodColor,
-        },
-        markingEnd: {
-          endingDay: true,
-          color: periodColor,
-        },
-        betweenDays: [],
-      }
-      const daysBetweenStartEnd = getDaysDifference(startDate, endDate)
-      for (let i = 1; i < daysBetweenStartEnd; i++) {
-        const betweenDay = {
-          date: formatDate(addDays(startDate, i)),
-          marking: {
-            color: periodColor,
-          },
-        }
-        eventObject.betweenDays.push(betweenDay)
-      }
-      return eventObject
+      // } else {
+      //   const eventObject = {
+      //     type: 'period',
+      //     dateStart: startFormattedString,
+      //     dateEnd: endFormattedString,
+      //     markingStart: {
+      //       startingDay: true,
+      //       color: periodColor,
+      //     },
+      //     markingEnd: {
+      //       endingDay: true,
+      //       color: periodColor,
+      //     },
+      //     betweenDays: [],
+      //   }
+      //   const daysBetweenStartEnd = getDaysDifference(startDate, endDate)
+      //   for (let i = 1; i < daysBetweenStartEnd; i++) {
+      //     const betweenDay = {
+      //       date: formatDate(addDays(startDate, i)),
+      //       marking: {
+      //         color: periodColor,
+      //       },
+      //     }
+      //     eventObject.betweenDays.push(betweenDay)
+      //   }
+      //   return eventObject
     }
   }
 
@@ -184,7 +249,8 @@ export default function RefactoredCalendar(props) {
         {
           _id: assignment._id,
           title: title,
-          color: startFormattedString == endFormattedString ? dotColor : periodColor
+          color:
+            startFormattedString == endFormattedString ? dotColor : periodColor,
         },
       ],
     }
@@ -208,14 +274,20 @@ export default function RefactoredCalendar(props) {
   const [selected, setSelected] = useState('')
 
   const renderItem = useCallback(({ item }) => {
-    return <AgendaItem item={item} dotColor={item.color} onPress={() => console.log('szerkesztés')} onItemPress={() => console.log(item)}/>
+    return (
+      <AgendaItem
+        item={item}
+        dotColor={item.color}
+        onPress={() => console.log('szerkesztés')}
+        onItemPress={() => navigation.navigate('EditAssignment', { id: item._id })}
+      />
+    )
   }, [])
 
   LocaleConfig.defaultLocale = 'hu'
   return (
     <>
-      <CalendarProvider
-      >
+      <CalendarProvider>
         <Calendar
           //current={Date.now()}
           onDayPress={(day) => {
@@ -228,7 +300,18 @@ export default function RefactoredCalendar(props) {
           theme={calendarTheme}
           firstDay={1}
           markingType={'period'}
-          markedDates={markedDays}
+          markedDates={{
+            '2024-02-15': {color:periodColor, startingDay: true, endingDay: true},
+            '2024-02-18': {color:periodColor, startingDay: true, endingDay: true, dotColor: dotColor, marked: true},
+            //'2024-02-18': {dotColor: dotColor, marked: true},
+            '2024-02-19': {marked: true, dotColor: dotColor},
+            '2024-02-20': {marked: true, dotColor: dotColor},
+            '2024-02-21': {marked: true, dotColor: dotColor, startingDay: true, color: periodColor},
+            '2024-02-22': {endingDay: true, color: periodColor},
+            '2024-02-28': {startingDay: true, color: periodColor},
+            '2024-02-29': {endingDay: true, color: periodColor, startingDay: false},
+
+          }}
           leftArrowImageSource={leftArrowIcon}
           rightArrowImageSource={rightArrowIcon}
         />
