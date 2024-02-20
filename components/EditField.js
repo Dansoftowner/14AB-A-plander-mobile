4a5b8fde831aea84b1cd86a995cbea800809f223
KubikArmin <kubikarminmartin@gmail.com>
2024-02-20 21:56:17 +0100

    :lipstick: Make EditField reusable

--- components/EditField.js
+++ components/EditField.js
@@ -3,12 +3,28 @@ import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import {useTheme} from '@react-navigation/native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-function EditField({onPress, style}) {
+function EditField({onPress, style, type, themeColor}) {
     const { colors: colorsByTheme } = useTheme()  
+
+    const typeProps = {
+      delete: {
+        color: colorsByTheme.medium_red_light_red,
+        icon: 'trash-can',
+      },
+      edit: {
+        color: colorsByTheme.white_dark_blue,
+        icon: 'pencil',
+      },
+      add: {
+        color: colorsByTheme.medium_green_light_green,
+        icon: 'plus'
+      }
+    }
+
     return (
-    <View style={[styles.container, {backgroundColor: colorsByTheme.medium_blue_yellow}, style]}>
+    <View style={[styles.container, {backgroundColor: typeProps[type].color}, style]}>
       <TouchableWithoutFeedback onPress={onPress} >
-        <MaterialCommunityIcons name="pencil" size={15} color={colorsByTheme.white_dark_blue} />
+        <MaterialCommunityIcons name={typeProps[type].icon} size={15} color={colorsByTheme.white_dark_blue} />
       </TouchableWithoutFeedback>
     </View>
   );
