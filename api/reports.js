eb28643bfe9132a8614127aa1132a1503df02292
KubikArmin <kubikarminmartin@gmail.com>
2024-02-23 22:37:42 +0100

    :sparkles: Added new request for delete report

--- api/reports.js
+++ api/reports.js
@@ -1,11 +1,36 @@
-import i18n from "../locales/i18n";
-import apiClient from "./client";
+import i18n from '../locales/i18n'
+import apiClient from './client'
 
 // const token = getToken();
 
 //const associations = await apiClient.get("/associations", { headers: { "x-auth-token": token } });
-const getReport = (id) => apiClient.get(`/assignments/${id}/report`);
+const getReport = (id) => apiClient.get(`/assignments/${id}/report`)
+const deleteReport = (id) => apiClient.delete(`/assignments/${id}/report`)
+
+const patchReport = (
+  id,
+  method,
+  purpose,
+  licensePlateNumber,
+  startKm,
+  endKm,
+  externalOrganization,
+  externalRepresentative,
+  description,
+) =>
+  apiClient.patch(`/assignments/${id}/report`, {
+    method,
+    purpose,
+    licensePlateNumber,
+    startKm,
+    endKm,
+    externalOrganization,
+    externalRepresentative,
+    description,
+  })
 
 export default {
-    getReport
+  getReport,
+  deleteReport,
+  patchReport
 }
