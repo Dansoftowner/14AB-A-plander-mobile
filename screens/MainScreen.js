010cca6c7af827957ba5f79b88d28b21a2336a25
KubikArmin <kubikarminmartin@gmail.com>
2024-02-25 22:27:49 +0100

    :recycle: Refactor MainScreen

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,30 +1,26 @@
 import React, { useContext } from 'react'
 import { View, StyleSheet } from 'react-native'
-import Screen from './Screen'
-import MyText from '../components/MyText'
 import { useTheme } from '@react-navigation/native'
-import colors from '../config/colors'
-import useAuth from '../auth/useAuth'
-import ThemeSwitch from '../components/ThemeSwitch'
-import LanguageSelector from '../components/LanguageSelector'
-import Logout from '../components/Logout'
+
+import MyText from '../components/MyText'
 import AuthContext from '../auth/authContext'
 
-function MainScreen(props) {
-  const {user} = useContext(AuthContext)
+function MainScreen() {
+  const { user } = useContext(AuthContext)
   const { colors: colorsByTheme } = useTheme()
 
   return (
-      <View style={[styles.container, 
+    <View
+      style={[
+        styles.container,
         { backgroundColor: colorsByTheme.medium_blue_dark_blue },
-      ]}>
-        <MyText style={[styles.text]}>
-          Üdv{'\n'}újra,{'\n'}{user.name.split(' ').map(word => word + '\n')}
-        </MyText>
-        <MyText>
-          {/* {JSON.stringify(user)} */}
-        </MyText>
-      </View>
+      ]}
+    >
+      <MyText style={[styles.text]}>
+        Üdv{'\n'}újra,{'\n'}
+        {user.name.split(' ').map((word) => word + '\n')}
+      </MyText>
+    </View>
   )
 }
 
@@ -32,13 +28,13 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
+    flex: 1,
   },
   text: {
     fontSize: 50,
     fontWeight: '700',
-    textTransform: 'uppercase'
-  }
+    textTransform: 'uppercase',
+  },
 })
 
 export default MainScreen
