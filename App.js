2072dfb1b8f552d2edba04dae98e34bc5b0b7d9d
KubikArmin <kubikarminmartin@gmail.com>
2024-02-25 21:47:29 +0100

    :recycle: Refactored App.js

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 import 'react-native-gesture-handler'
 import React, { useState } from 'react'
-import { Appearance } from 'react-native'
+
+import { useColorScheme } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
-import { useColorScheme } from 'react-native'
 
 import i18n from './locales/i18n'
 
 import AppDarkTheme from './config/themes/appDarkTheme'
 import AppLightTheme from './config/themes/appLightTheme'
 import AuthContext from './auth/authContext'
-import MainScreen from './screens/MainScreen'
 import MyDrawer from './navigation/MyDrawer'
 import MyStack from './navigation/MyStack'
-import LoginScreen from './screens/LoginScreen'
 import LanguageContext from './locales/LanguageContext'
+import LoginScreen from './screens/LoginScreen'
+import MainScreen from './screens/MainScreen'
 
 export default function App() {
   const colorScheme = useColorScheme()
   const [user, setUser] = useState()
   const [language, setLanguage] = useState('hu')
   i18n.locale = language
-  //Appearance.setColorScheme('light')
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <AuthContext.Provider value={{ user, setUser }}>
