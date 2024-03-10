fa1e3430f6b3d1bd598ba63e0c376b9ccff402aa
KubikArmin <kubikarminmartin@gmail.com>
2024-03-10 17:22:01 +0100

    :lipstick: :recycle:  Refactored ViewPdfScreen

--- screens/ViewPdfScreen.js
+++ screens/ViewPdfScreen.js
@@ -1,10 +1,14 @@
 import { useRef, useState } from 'react';
-import { StyleSheet, SafeAreaView, Button, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { useTheme } from '@react-navigation/native'
+
 import Pdf from 'react-native-pdf';
-import Screen from './Screen';
-import reports from '../api/reports';
 import { Buffer } from 'buffer'
+
+import reports from '../api/reports';
+import i18n from '../locales/i18n';
+
+import Screen from './Screen';
 import MyText from '../components/MyText';
 
 function ViewPdfScreen({ route }) {
@@ -14,10 +18,8 @@ function ViewPdfScreen({ route }) {
         if (!response.ok) {
             return
         }
-        
         const buf = Buffer.from(response.data)
         const base64 = buf.toString('base64')
-        
         setPdfSource({ uri: `data:application/pdf;base64,${base64}`, cache: true });
     }
 
@@ -26,7 +28,7 @@ function ViewPdfScreen({ route }) {
     }, [route.params.id])
 
     const { colors: colorsByTheme } = useTheme()
-    const [pdfSource, setPdfSource] = useState({uri: '', cache: true});
+    const [pdfSource, setPdfSource] = useState({ uri: '', cache: true });
     const pdfRef = useRef();
     return (
         <Screen style={[
@@ -35,25 +37,17 @@ function ViewPdfScreen({ route }) {
         ]}
         >
             <Pdf
-            renderActivityIndicator={() => (<MyText style={{fontWeight: 'bold', fontSize: 25}}>
-                Loading...
-            </MyText>)}
+                renderActivityIndicator={() => (
+                    <MyText style={{ fontWeight: 'bold', fontSize: 25 }}>
+                        {i18n.t('loading')}
+                    </MyText>)}
                 trustAllCerts={false}
                 ref={pdfRef}
                 source={pdfSource}
-                onLoadComplete={(numberOfPages, filePath) => {
-                    console.log(`Number of pages: ${numberOfPages}`);
-                }}
-                onPageChanged={(page, numberOfPages) => {
-                    console.log(`Current page: ${page}`);
-                }}
                 onError={(error) => {
                     console.log(error);
                 }}
-                onPressLink={(uri) => {
-                    console.log(`Link pressed: ${uri}`);
-                }}
-                style={ [styles.pdf, {backgroundColor: colorsByTheme.Login_background}]}
+                style={[styles.pdf, { backgroundColor: colorsByTheme.Login_background }]}
             />
         </Screen>
     );
