7238bbe397075a1fd2373c98f92e275a2693982c
feherszabolcs <feherszabi2005@gmail.com>
2024-03-14 19:53:25 +0100

    :construction: fetch previous chats

--- screens/ChatsScreen.js
+++ screens/ChatsScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect, useCallback } from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { GiftedChat } from 'react-native-gifted-chat'
 import AuthContext from '../auth/authContext'
 
@@ -7,6 +7,8 @@ import hu from 'dayjs/locale/hu'
 
 import io from 'socket.io-client'
 import storage from '../auth/storage'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import apiClient from '../api/client'
 
 function ChatsScreen(props) {
   const socket = io('wss://dev-plander-org.koyeb.app', {
@@ -15,8 +17,35 @@ function ChatsScreen(props) {
       token: storage.getToken(),
     },
   })
-  const [messages, setMessages] = useState([])
+
   const { user } = useContext(AuthContext)
+
+  const { data, isFetched, isFetching } = useInfiniteQuery({
+    queryKey: ['chats'],
+    queryFn: async ({ pageParam = 1 }) => {
+      const res = await apiClient.get('/chats', {
+        limit: 10,
+        offset: (pageParam - 1) * 10,
+      })
+      return res.data
+    },
+    staleTime: 0,
+    refetchOnWindowFocus: false,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.items?.length > 0 ? allPages?.length + 1 : undefined
+    },
+  })
+
+  const chatMessages = data?.pages.reduce((acc, page) => {
+    return [...acc, ...page.items]
+  })
+
+  const [messages, setMessages] = useState([])
+  useEffect(() => {
+    let parsed = extractMessages(chatMessages, setMessages)
+    setMessages(parsed)
+    console.log(parsed)
+  }, [chatMessages, isFetched])
 
   useEffect(() => {
     socket.connect()
@@ -24,7 +53,7 @@ function ChatsScreen(props) {
 
   useEffect(() => {
     socket.on('recieve-message', (message) => {
-      if (message.sender._id != user._id) {
+      if (message.sender._id != user._id && !messages.includes(message)) {
         console.log(message)
         setMessages((prevMessage) =>
           GiftedChat.append(prevMessage, {
@@ -33,11 +62,11 @@ function ChatsScreen(props) {
         )
       }
     })
+    return () => socket.off('recieve-message')
   }, [])
 
   const onSend = useCallback((localMessages) => {
     socket.emit('send-message', localMessages[0].text)
-    console.log('local', localMessages[0])
     setMessages((prevMessage) =>
       GiftedChat.append(prevMessage, {
         ...localMessages[0],
@@ -46,18 +75,29 @@ function ChatsScreen(props) {
   }, [])
 
   return (
-    <GiftedChat
-      placeholder="Írja be az üzenetet..."
-      locale={hu}
-      messages={messages}
-      onSend={onSend}
-      user={user}
-      key={(message) => message?._id}
-      showUserAvatar={true}
-      bottomOffset={10}
-      renderUsernameOnMessage={true}
-      timeFormat="LT"
-    />
+    <>
+      {isFetching && <Text>Loading...</Text>}
+      <GiftedChat
+        placeholder="Írja be az üzenetet..."
+        locale={hu}
+        messages={messages}
+        onSend={onSend}
+        user={user}
+        key={(message) => message?._id}
+        showUserAvatar={true}
+        bottomOffset={10}
+        renderUsernameOnMessage={true}
+        timeFormat="LT"
+        loadEarlier
+        infiniteScroll
+        scrollToBottom
+        renderChatEmpty={() => (
+          <View style={{ height: 100 }}>
+            <Text>Nem történt még beszélgetés!</Text>
+          </View>
+        )}
+      />
+    </>
   )
 }
 
@@ -71,6 +111,15 @@ function convertToGiftedChatMessage(message) {
     text: message.content,
     createdAt: message.timestamp,
     user: message.sender,
+  }
+}
+
+const extractMessages = (messages, setMessages) => {
+  if (messages != undefined) {
+    const parsed = messages.items.map((r) => convertToGiftedChatMessage(r))
+    setMessages(parsed)
+
+    return parsed
   }
 }
 
